62772b801e69e95d2cb10dfe07a0451d98e33216
AgaSzyma <126581997+AgaSzyma@users.noreply.github.com>
2023-04-07 15:28:27 +0200

    Prymitywna skrzynia ze skarbem

---
+++ Game/Items/Treasure.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Game.Items;
+using Game.Windows;
+
+namespace Game.Creatures
+{
+    public class Treasure : GameSprite
+    {
+        public Treasure(Vector2 Position, int Width, int Height) : base(Position, Width, Height)
+        {
+            this.Position = Position;
+            this.Width = Width;
+            this.Height = Height;
+        }
+
+        public void Create(Canvas gameArea)
+        {
+            Body = new Rectangle();
+            gameArea.Children.Add(Body);
+            Body.Width = this.Width;
+            Body.Height = this.Height;
+            Canvas.SetTop(Body, Position.Y);
+            Canvas.SetLeft(Body, Position.X);
+            Body.Fill = new SolidColorBrush(Colors.Brown);
+        }
+
+        public void Draw()
+        {
+            Canvas.SetLeft(Body, Position.X);
+            Canvas.SetTop(Body, Position.Y);
+        }
+
+        public void Delete(Canvas gameArea)
+        {
+            gameArea.Children.Remove(Body);
+        }
+    }
+
+
+    public class Coin : GameSprite
+    {
+        public bool czyUsunac = false;
+        public Coin(Vector2 Position, int Width, int Height) : base(Position, Width, Height)
+        {
+            this.Position = Position;
+            this.Width = Width;
+            this.Height = Height;
+        }
+
+        public void Create(Canvas gameArea)
+        {
+            Body = new Rectangle();
+            gameArea.Children.Add(Body);
+            Body.Width = this.Width;
+            Body.Height = this.Height;
+            Canvas.SetTop(Body, Position.Y);
+            Canvas.SetLeft(Body, Position.X);
+            Body.Fill = new SolidColorBrush(Colors.Yellow);
+        }
+
+        public void Draw()
+        {
+            Canvas.SetLeft(Body, Position.X);
+            Canvas.SetTop(Body, Position.Y);
+        }
+
+        public void Delete(Canvas gameArea)
+        {
+            gameArea.Children.Remove(Body);
+        }
+    }
+
+    public class Emerald : GameSprite
+    {
+        public bool czyUsunac = false;
+        public Emerald(Vector2 Position, int Width, int Height) : base(Position, Width, Height)
+        {
+            this.Position = Position;
+            this.Width = Width;
+            this.Height = Height;
+        }
+
+        public void Create(Canvas gameArea)
+        {
+            Body = new Rectangle();
+            gameArea.Children.Add(Body);
+            Body.Width = this.Width;
+            Body.Height = this.Height;
+            Canvas.SetTop(Body, Position.Y);
+            Canvas.SetLeft(Body, Position.X);
+            Body.Fill = new SolidColorBrush(Colors.Green);
+        }
+
+        public void Draw()
+        {
+            Canvas.SetLeft(Body, Position.X);
+            Canvas.SetTop(Body, Position.Y);
+        }
+
+        public void Delete(Canvas gameArea)
+        {
+            gameArea.Children.Remove(Body);
+        }
+    }
+
+    public class Ruby : GameSprite
+    {
+        public bool czyUsunac = false;
+        public Ruby(Vector2 Position, int Width, int Height) : base(Position, Width, Height)
+        {
+            this.Position = Position;
+            this.Width = Width;
+            this.Height = Height;
+        }
+
+        public void Create(Canvas gameArea)
+        {
+            Body = new Rectangle();
+            gameArea.Children.Add(Body);
+            Body.Width = this.Width;
+            Body.Height = this.Height;
+            Canvas.SetTop(Body, Position.Y);
+            Canvas.SetLeft(Body, Position.X);
+            Body.Fill = new SolidColorBrush(Colors.Red);
+        }
+
+        public void Draw()
+        {
+            Canvas.SetLeft(Body, Position.X);
+            Canvas.SetTop(Body, Position.Y);
+        }
+
+        public void Delete(Canvas gameArea)
+        {
+            gameArea.Children.Remove(Body);
+        }
+    }
+
+    public class Diamond : GameSprite
+    {
+        public bool czyUsunac = false;
+        public Diamond(Vector2 Position, int Width, int Height) : base(Position, Width, Height)
+        {
+            this.Position = Position;
+            this.Width = Width;
+            this.Height = Height;
+        }
+
+        public void Create(Canvas gameArea)
+        {
+            Body = new Rectangle();
+            gameArea.Children.Add(Body);
+            Body.Width = this.Width;
+            Body.Height = this.Height;
+            Canvas.SetTop(Body, Position.Y);
+            Canvas.SetLeft(Body, Position.X);
+            Body.Fill = new SolidColorBrush(Colors.Blue);
+        }
+
+        public void Draw()
+        {
+            Canvas.SetLeft(Body, Position.X);
+            Canvas.SetTop(Body, Position.Y);
+        }
+
+        public void Delete(Canvas gameArea)
+        {
+            gameArea.Children.Remove(Body);
+        }
+    }
+}
