3e9cb1fd472eb1445c841662a93fec1401c55f24
AgaSzyma <126581997+AgaSzyma@users.noreply.github.com>
2023-06-06 04:01:26 +0200

    Highscore wersja 3

--- Game/Windows/Window_Score.xaml.cs
+++ Game/Windows/Window_Score.xaml.cs
@@ -10,10 +10,11 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
 using Game.Creatures;
-
+using Game.UserControls;
 namespace Game.Windows
 {
     /// <summary>
